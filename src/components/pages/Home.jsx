175664c8bdc5d4dc6288e46491d62d9013f046bc
Vignesh1hp <vigneshkumarb.sc2004@gmail.com>
2025-07-29 15:08:57 +0530

    created a star rating

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,6 +1,29 @@
-import React from "react";
+import React, { use, useState } from "react";
+import { FaStar } from "react-icons/fa";
+
+const colors = {
+  orange: "#FFBA5A",
+  grey: "#a9a9a9",
+};
 
 const Home = () => {
+  const stars = Array(5).fill(0);
+
+  const [currentValue, setCurrentValue] = useState(0);
+
+  const [hoverValue, setHoverValue] = useState(undefined);
+
+  const handleClick = (value) => {
+    setCurrentValue(value);
+  };
+
+  const handleMouseHover = (value) => {
+    setHoverValue(value);
+  };
+
+  const handleMouseLeave = (value) => {
+    setHoverValue(undefined);
+  };
   return (
     <div className="container">
       <h1>FeedBack Form</h1>
@@ -27,6 +50,33 @@ const Home = () => {
 
             <option value="generalFeedback">General Feedback</option>
           </select>
+        </div>
+
+        <div className="star">
+          {stars.map((_, index) => {
+            return (
+              <FaStar
+                key={index}
+                size={24}
+                style={{
+                  marginRight: 10,
+                  cursor: "pointer",
+                }}
+                color={
+                  (hoverValue || currentValue) > index
+                    ? colors.orange
+                    : colors.grey
+                }
+                onClick={() => {
+                  handleClick(index + 1);
+                }}
+                onMouseOver={()=>{
+                  handleMouseHover(index+1)
+                }}
+                onMouseLeave={handleMouseLeave}
+              />
+            );
+          })}
         </div>
       </form>
     </div>
